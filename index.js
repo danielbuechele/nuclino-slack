892a4d377fd6040145beefb3c968eb12214f75f0
Daniel Büchele <daniel@buechele.cc>
2020-02-15 17:36:49 +0000

    node upgrade

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function updateToken() {
       const match = cookie.match(/token=([A-Za-z0-9+-\._]+)/);
       if (match && match.length > 0) {
         token = match[1];
-        fs.writeFile(TOKEN_PATH, token, 'utf8');
+        fs.writeFile(TOKEN_PATH, token, 'utf8', () => {});
       }
       return token;
     });
